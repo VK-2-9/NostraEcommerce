2b9a85f661a6287f60d70d1e4b3e2c93f7561394
VK-2-9 <vishalnath002@gmail.com>
2025-03-14 04:16:09 +0530

    done

--- index.js
+++ index.js
@@ -19,3 +19,19 @@ menuOpen.addEventListener("click",function(){
 menuClose.addEventListener("click",function(){
     sidenav.style.left="-50%";
 })
+
+
+//slider section
+
+var sliderImages=document.getElementById("slider-images");
+var img1=document.getElementById("img1");
+var img2=document.getElementById("img2");
+var img3=document.getElementById("img3");
+var sliderLeft=document.getElementById("slider-left");
+var sliderRight=document.getElementById("slider-right");
+
+
+sliderRight.addEventListener("click",function(){
+    var clickedImage= sliderRight.closest("img");
+    console.log(clickedImage);
+})
