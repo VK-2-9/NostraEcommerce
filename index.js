160821268b205e28a4e1cd2a556188439b0421da
VK-2-9 <vishalnath002@gmail.com>
2025-03-13 06:38:03 +0530

    creating

---
+++ index.js
@@ -0,0 +1,8 @@
+// Closing the offer section
+
+var offer=document.getElementById("offer");
+var offerClose=document.getElementById("offer-close");
+
+offerClose.addEventListener("click",function(){
+    offer.style.display="none"
+})
