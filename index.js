8dd2885e46a91bdd6edffb7b9ac16ef21728fe67
VK-2-9 <vishalnath002@gmail.com>
2025-03-13 20:12:28 +0530

    index

--- index.js
+++ index.js
@@ -6,3 +6,16 @@ var offerClose=document.getElementById("offer-close");
 offerClose.addEventListener("click",function(){
     offer.style.display="none"
 })
+
+//sidenav
+var menuOpen=document.getElementById("menu-open");
+var sidnav=document.getElementById("sidenav");
+var menuClose=document.getElementById("menu-close");
+
+menuOpen.addEventListener("click",function(){
+    sidenav.style.left="0";
+})
+
+menuClose.addEventListener("click",function(){
+    sidenav.style.left="-50%";
+})
