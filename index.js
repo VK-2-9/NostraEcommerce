e5a47f354300761848457cd09bb7c8edc8c3c9de
VK-2-9 <vishalnath002@gmail.com>
2025-03-19 20:05:56 +0530

    a

--- index.js
+++ index.js
@@ -1,37 +1,88 @@
 // Closing the offer section
 
-var offer=document.getElementById("offer");
-var offerClose=document.getElementById("offer-close");
+var offer = document.getElementById("offer");
+var offerClose = document.getElementById("offer-close");
 
-offerClose.addEventListener("click",function(){
-    offer.style.display="none"
-})
+offerClose.addEventListener("click", function () {
+  offer.style.display = "none";
+});
 
 //sidenav
-var menuOpen=document.getElementById("menu-open");
-var sidnav=document.getElementById("sidenav");
-var menuClose=document.getElementById("menu-close");
+var menuOpen = document.getElementById("menu-open");
+var sidnav = document.getElementById("sidenav");
+var menuClose = document.getElementById("menu-close");
 
-menuOpen.addEventListener("click",function(){
-    sidenav.style.left="0";
-})
+menuOpen.addEventListener("click", function () {
+  sidenav.style.left = "0";
+});
 
-menuClose.addEventListener("click",function(){
-    sidenav.style.left="-50%";
-})
-
+menuClose.addEventListener("click", function () {
+  sidenav.style.left = "-50%";
+});
 
 //slider section
 
-var sliderImages=document.getElementById("slider-images");
-var img1=document.getElementById("img1");
-var img2=document.getElementById("img2");
-var img3=document.getElementById("img3");
-var sliderLeft=document.getElementById("slider-left");
-var sliderRight=document.getElementById("slider-right");
+// Collection secton
+
+// Search function
+var searchBox = document.getElementById("search-box");
+var collectionContainer = document.querySelectorAll(".collection-container");
+
+searchBox.addEventListener("keyup", function () {
+  collectionContainer.forEach((container)=> {
+    var dressName = container.querySelector("h1").innerHTML.toUpperCase();
+    var enteredText = searchBox.value.toUpperCase();
+    if (dressName.includes(enteredText)) {
+      container.style.display = "block";
+    } else {
+      container.style.display = "none";
+    }
+  });
+});
 
 
-sliderRight.addEventListener("click",function(){
-    var clickedImage= sliderRight.closest("img");
-    console.log(clickedImage);
+
+//Check function
+
+var collectionContainer= document.querySelectorAll(".collection-container");
+var check=document.getElementsByName("checkbox");
+var newProducts=[]
+
+check.forEach((check)=>{
+  check.addEventListener("click",()=>{
+
+    if (check.checked){
+
+      collectionContainer.forEach(container=>{
+        var tags=container.querySelector("tags");
+        var tagsText=tags.innerHTML;
+        if(tagsText.includes(check.value)){
+            newProducts.push(container)
+            console.log(newProducts)
+            collectionContainer.innerHTML=newProducts
+        }
+       
+      })
+  
+    }
+
+  })
 })
+
+
+
+
+  
+  
+
+
+
+
+
+
+
+
+
+
+
+
